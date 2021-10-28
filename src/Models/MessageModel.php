c1a7575efd7a341aa2dfb4b51874f694a8ab730e
Adam <adam@adammalone.net>
2021-10-28 23:14:58 +1100

    We can manually create messages so make all of these optional. We might need to add in checking when we do a send message though. TBD.

--- src/Models/MessageModel.php
+++ src/Models/MessageModel.php
@@ -6,7 +6,7 @@ class MessageModel extends BaseModel
 {
 
     public $id;
-    public $orgId;
+    public $userId;
     public $body;
     public $author;
     public $type;
@@ -18,15 +18,35 @@ class MessageModel extends BaseModel
 
     public function createModel($message)
     {
-        $this->id = $message->id;
-        // $this->orgId = $message->orgId;
-        $this->body = $message->body;
-        $this->author = $message->author;
-        $this->type = $message->type;
-        $this->conversationId = $message->conversationId;
-        $this->createdAt = $message->createdAt;
-        $this->buttons = $message->buttons;
-        $this->context = $message->context;
-        $this->attributes = $message->attributes;
+        if ($this->id) {
+            $this->id = $message->id;
+        }
+        if ($this->userId) {
+            $this->userId = $message->userId;
+        }
+        if ($this->body) {
+            $this->body = $message->body;
+        }
+        if ($this->author) {
+            $this->author = $message->author;
+        }
+        if ($this->type) {
+            $this->type = $message->type;
+        }
+        if ($this->conversationId) {
+            $this->conversationId = $message->conversationId;
+        }
+        if ($this->createdAt) {
+            $this->createdAt = $message->createdAt;
+        }
+        if ($this->buttons) {
+            $this->buttons = $message->buttons;
+        }
+        if ($this->context) {
+            $this->context = $message->context;
+        }
+        if ($this->attributes) {
+            $this->attributes = $message->attributes;
+        }
     }
 }
